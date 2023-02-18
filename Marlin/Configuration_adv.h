ea41869d777b7964407e07c039348f9fb83cf4a1
Luiz Borges <luizbgomide@gmail.com>
2023-02-17 21:34:52 -0300

    Habilita o comando para ligar autoretract

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2525,9 +2525,9 @@
  */
 #define FWRETRACT
 #if ENABLED(FWRETRACT)
-  // #define FWRETRACT_AUTORETRACT             // Override slicer retractions
+  #define FWRETRACT_AUTORETRACT             // Override slicer retractions
   #if ENABLED(FWRETRACT_AUTORETRACT)
-    #define MIN_AUTORETRACT             0.1 // (mm) Don't convert E moves under this length
+    #define MIN_AUTORETRACT             1.0 // (mm) Don't convert E moves under this length
     #define MAX_AUTORETRACT            10.0 // (mm) Don't convert E moves over this length
   #endif
   #define RETRACT_LENGTH                5   // (mm) Default retract length (positive value)
