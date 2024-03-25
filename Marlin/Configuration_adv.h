3cdbfdd862c0bd150b2595ed9c93c4622f066b5b
Luiz Borges <luizbgomide@gmail.com>
2024-03-24 22:35:41 -0300

    Removendo o Z Offset da configuração e alterando os fans para hardware PWM

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -565,8 +565,8 @@
  * gets it spinning reliably for a short time before setting the requested speed.
  * (Does not work on Sanguinololu with FAN_SOFT_PWM.)
  */
-#define FAN_KICKSTART_TIME  250  // (ms)
-#define FAN_KICKSTART_POWER 180  // 64-255
+#define FAN_KICKSTART_TIME  500  // (ms)
+#define FAN_KICKSTART_POWER 200  // 64-255
 
 // Some coolers may require a non-zero "off" state.
 //#define FAN_OFF_PWM  1
@@ -584,7 +584,7 @@
  * Define one or both of these to override the default 0-255 range.
  */
 #define FAN_MIN_PWM 50
-#define FAN_MAX_PWM 250
+#define FAN_MAX_PWM 254
 
 /**
  * Fan Fast PWM
