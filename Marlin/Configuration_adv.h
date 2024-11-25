aa63efb8a030ecd0a1b6c77d44f99bef9958c5a4
Luiz Borges <luizbgomide@gmail.com>
2024-11-25 18:33:23 -0300

    Testando mais configurações do fan

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -583,8 +583,8 @@
  *
  * Define one or both of these to override the default 0-255 range.
  */
-#define FAN_MIN_PWM 64
-#define FAN_MAX_PWM 254
+#define FAN_MIN_PWM 128
+#define FAN_MAX_PWM 255
 
 /**
  * Fan Fast PWM
